6bae85b1eba056e0870beeadb03c23e9145b99a5
Luke Gartner <lukegartner4@gmail.com>
2024-06-04 15:06:19 -0500

    completes counter challenge using client component

--- app/client/page.js
+++ app/client/page.js
@@ -1,7 +1,27 @@
+"use client";
+import { useState } from "react";
+
 const ClientPage = () => {
+  const [count, setCount] = useState(0);
+  console.log("client component");
   return (
     <div>
-      <h1 className="text-7xl">Client Page</h1>
+      <h1 className="text-7xl font-bold mb-4">{count}</h1>
+      <button
+        className="btn btn-primary mx-2"
+        onClick={() => setCount(count + 1)}
+      >
+        increase
+      </button>
+      <button
+        className="btn btn-primary mx-2"
+        onClick={() => setCount(count - 1)}
+      >
+        decrease
+      </button>
+      <button className="btn btn-secondary mx-2" onClick={() => setCount(0)}>
+        reset
+      </button>
     </div>
   );
 };
