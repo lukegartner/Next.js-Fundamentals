a5769760720832d6c5b6fdc802db4f2a98183597
Luke Gartner <lukegartner4@gmail.com>
2024-06-04 18:13:16 -0500

    adds remote images

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const getSingleDrink = async (id) => {
@@ -19,6 +20,14 @@ const SingleDrinkPage = async ({ params }) => {
       <Link href="/drinks" className="btn btn-primary mt-8 mb-12">
         Back to drinks
       </Link>
+      <Image
+        src={imgSrc}
+        width={300}
+        height={300}
+        className="w-48 h-48 rounded-lg shadow-lg mb-4"
+        priority
+        alt={title}
+      />
       <h1 className="text-4xl mb-8">{title}</h1>
     </div>
   );
