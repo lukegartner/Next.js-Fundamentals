d7900d4025d5b69c785ade62e5e6d8be0515f304
Luke Gartner <lukegartner4@gmail.com>
2024-06-04 16:47:46 -0500

    adds loading graphic

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,8 +1,16 @@
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
 
-const DrinksPage = async () => {
+const fetchDrinks = async () => {
+  // Creates delay to see loading in action
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   const response = await fetch(url);
   const data = await response.json();
+  return data;
+};
+
+const DrinksPage = async () => {
+  const data = await fetchDrinks();
   console.log(data);
   return (
     <div>
