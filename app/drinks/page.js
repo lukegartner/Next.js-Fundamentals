9cd1419951c3e0c794dc7b18e4f12aaf2052e15a
Luke Gartner <lukegartner4@gmail.com>
2024-06-04 16:55:32 -0500

    adds error component

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -5,6 +5,11 @@ const fetchDrinks = async () => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
   const response = await fetch(url);
+  // throw error
+  if (!response.ok) {
+    throw new Error("Failed to fetch drinks");
+  }
+
   const data = await response.json();
   return data;
 };
