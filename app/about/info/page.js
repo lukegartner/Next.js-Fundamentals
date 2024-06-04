21abcc5998e6f48406e244e0144d47cfedf2e87a
Luke Gartner <lukegartner4@gmail.com>
2024-06-04 11:58:27 -0500

    initial commit

---
+++ app/about/info/page.js
@@ -0,0 +1,5 @@
+const AboutInfo = () => {
+  return <div className="text-3xl">About Info Page</div>;
+};
+
+export default AboutInfo;
---
+++ app/prisma-example/page.js
@@ -0,0 +1,9 @@
+const PrismaExample = () => {
+  return (
+    <div>
+      <h1 className="text-7xl">Prisma Example</h1>
+    </div>
+  );
+};
+
+export default PrismaExample;
